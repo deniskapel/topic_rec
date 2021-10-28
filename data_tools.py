800cd5463ffc48e78f532dfc22dc8f68d02d42a9
deniskapel <8568400+deniskapel@users.noreply.github.com>
2021-10-28 23:50:57 +0300

    Update Sequence generator to work on a provided mapping of tokens

--- data_tools.py
+++ data_tools.py
@@ -8,7 +8,7 @@ from tqdm.notebook import tqdm
 
 class Chunker():
     # Class to extract noun chunks from dialogues
-    
+
     def __init__(self, spacy_model, stop_words):
         self.counter = Counter()
         self.stops = stop_words
@@ -20,9 +20,9 @@ class Chunker():
         return [d for d in dials if d]
 
     def __extract_chunks(self, chunked_utterance: list) -> list:
-        """ 
-            extract topic and necessary chunk information from 
-            every meaningfull noun chunk in the utternace 
+        """
+            extract topic and necessary chunk information from
+            every meaningfull noun chunk in the utternace
         """
         output = []
         for chunk in chunked_utterance:
@@ -33,12 +33,12 @@ class Chunker():
 
             output.append(chunk)
         return output
-    
+
     def parse_by_utterance(self, dialogue:str, delim:str = '__eou__') -> list:
-        """ 
+        """
             use spacy parser to extract noun_chunks
             parse a dialogue by utterance to improve extraction
-            
+
             returns list of Spacy Span instance
         """
         # split for better parsing
@@ -49,9 +49,9 @@ class Chunker():
         return [ut for ut in output if ut]
 
     def filter_chunks(self, dialogue: list, to_remove: list):
-        """ 
+        """
             filter out noun chunks
-            to_remove s is a list of chunks to remove, e.g. top most common    
+            to_remove s is a list of chunks to remove, e.g. top most common
         """
         return [chunk for chunk in dialogue if chunk.lower_ not in to_remove]
 
@@ -60,8 +60,8 @@ class Vectorizer():
     """
     Class to vectorize dialogues best on the vector of the chunks last word
     dialogue is an averaged vector of all noun chunks [their last word]
-    """ 
-    
+    """
+
     def __init__(self, num_docs: int):
         self.num_docs = num_docs
 
@@ -69,14 +69,14 @@ class Vectorizer():
         """  indexes docs and return vector for each doc """
         vec = np.zeros((self.num_docs, 300))
         doc2id = pd.DataFrame({'doc': docs})
-        
+
         for i, doc in enumerate(docs):
             vec[i] = self.__data_generation(doc)
 
         return vec, doc2id
 
     def __data_generation(self, doc) -> tuple:
-        """ 
+        """
             parse doc to return a tuple topics, vector
             returns list of topics
         """
@@ -95,33 +95,42 @@ class SequenceGenerator():
     Class to generate prev2next sequences of chunks
     if some seq of different sized, leave only the ones of the same size
     or pad
-    """ 
-    def __init__(self, seq_len: int):
+    seq_len: int indicating the length of the sequence to use for prediction
+    chunk2id and id2chunk: mappings of spaCy tokens and their index.
+
+    Use the same chunk2id and id2chunk that you will pass to TokenDataset.
+    OOV is handled by spacy and this dictionary is used only to extract vectors
+    """
+    def __init__(
+        self, seq_len: int, chunk2id: dict, id2chunk: list):
         # lengths of the sequence to predict the next
         self.seq_len = seq_len
-        self.chunk2id = {}
-        self.id2chunk = []
+        self.chunk2id = chunk2id
+        self.id2chunk = id2chunk
 
-    def get_sequences(self, docs: pd.DataFrame, seq_len = 3) -> pd.DataFrame:
+    def get_sequences(self, docs: pd.DataFrame) -> pd.DataFrame:
+        """
+        function generating sequences from rows of pandas dataset
+
+        docs: each or is a list of tokens (str, Spacy noun chunks, etc.)
+        update_vocab: boolean controlling whether to add new vocabulary or not
+        """
         seqs = []
         seqs.extend(docs['doc'].apply(self.generate_seq))
         seqs = [seq for seq in chain(*seqs)]
-        self.id2chunk = list(set([chunk for chunk in chain(*seqs)]))
-        self.chunk2id = {chunk: i for i, chunk in enumerate(self.id2chunk)}
+
         # replace chunks with their ids
         sequences = []
         for seq in seqs:
-            # to be modified for pre-sequences larger than 1
-            pre_ids = [self.chunk2id[chunk_id] for chunk_id in seq[:-1]]
-            post_ids = [self.chunk2id[chunk_id] for chunk_id in seq[1:]]
-            
-            sequences.append([pre_ids, post_ids])
-        
+            # handle oov
+            ids = [self.chunk2id.get(chunk_id, 0) for chunk_id in seq]
+            sequences.append([ids[:-1], ids[1:]])
+
         return pd.DataFrame(sequences, columns=['seq', 'target'])
-    
-    
+
+
     def generate_seq(self, row) -> tuple:
-        """ 
+        """
         from a given doc generates a sequence of prev tokens and the next one
         """
         sequences = []
